a8eb8fbd24927262742108a71b83c7ff19d77828
rlmikhailov <rl.mikhailov1996@gmail.com>
2025-09-16 20:33:24 +0900

    Mobile layout and more

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://rlmikhailov.github.io',
-  base: '/lawyer/', // Use '/' if you deploy to <username>.github.io root repo
+  base: '/', // Use '/' if you deploy to <username>.github.io root repo
 });
